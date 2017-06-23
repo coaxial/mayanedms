c72edc82b4c72371362653fbaef356d95b7eab6f
coaxial <hi@64b.it>
2017-06-23 11:46:58 -0400

    Add redirection

--- tarsnap-mayan-edms-backup.sh
+++ tarsnap-mayan-edms-backup.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 # As per Mayan EDMS documentation for sqlite3 setups (cf
 # https://bitbucket.org/r.rosario/mayan-edms-docker/src/4efe54e16538f5b605ad25a20f8560492ca61828/README.md)
+
+# User variables
+email=my_email@example.net
+tarsnap_output_filename=/tmp/tarsnap-output-temporary.log
+
 datavolpath="$(docker volume inspect mayan_data | jq '.[0].Mountpoint' | sed \
     -e "s/\"//g")"; \
   docker stop mayan-edms &&\
   tarsnap -c -f "mayan-edms-$(date +%Y-%m-%d_%H-%M-%S)" \
     "$datavolpath/document_storage" \
     "$datavolpath/db.sqlite3" \
-    "$datavolpath/settings" &&\
+    "$datavolpath/settings" \
+    >$tarsnap_output_filename 2>&1 &&\
   docker start mayan-edms
-
-# User variables
-email=my_email@example.net
-tarsnap_output_filename=/tmp/tarsnap-output-temporary.log
 
 # Send email
 if [ $? -eq 0 ]; then
