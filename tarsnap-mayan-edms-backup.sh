80a7570585fb5fba48714ff615f7419ad58f064d
coaxial <hi@64b.it>
2017-06-23 09:03:58 -0400

    Indentation

--- tarsnap-mayan-edms-backup.sh
+++ tarsnap-mayan-edms-backup.sh
@@ -2,6 +2,10 @@
 # As per Mayan EDMS documentation for sqlite3 setups (cf
 # https://bitbucket.org/r.rosario/mayan-edms-docker/src/4efe54e16538f5b605ad25a20f8560492ca61828/README.md)
 datavolpath="$(docker volume inspect mayan_data | jq '.[0].Mountpoint' | sed \
-  -e "s/\"//g")"; tarsnap -c -f "mayan-edms-$(date +%Y-%m-%d_%H-%M-%S)" \
-  "$datavolpath/document_storage" "$datavolpath/db.sqlite3" \
-  "$datavolpath/settings"
+    -e "s/\"//g")"; \
+  docker stop mayan-edms &&\
+  tarsnap -c -f "mayan-edms-$(date +%Y-%m-%d_%H-%M-%S)" \
+    "$datavolpath/document_storage" \
+    "$datavolpath/db.sqlite3" \
+    "$datavolpath/settings" &&\
+  docker start mayan-edms
