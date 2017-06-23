e9e328e341a0e42a0c661b03ed5a0f5e0bfb3879
coaxial <hi@64b.it>
2017-06-22 23:26:21 -0400

    Fix line returns

--- tarsnap-mayan-edms-backup.sh
+++ tarsnap-mayan-edms-backup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # As per Mayan EDMS documentation for sqlite3 setups (cf
 # https://bitbucket.org/r.rosario/mayan-edms-docker/src/4efe54e16538f5b605ad25a20f8560492ca61828/README.md)
-datavolpath="$(docker volume inspect mayan_data | jq '.[0].Mountpoint' | sed -e
-"s/\"//g")"; tarsnap -c -f "mayan-edms-$(date +%Y-%m-%d_%H-%M-%S)"
-"$datavolpath/document_storage" "$datavolpath/db.sqlite3"
-"$datavolpath/settings"
+datavolpath="$(docker volume inspect mayan_data | jq '.[0].Mountpoint' | sed \
+  -e "s/\"//g")"; tarsnap -c -f "mayan-edms-$(date +%Y-%m-%d_%H-%M-%S)" \
+  "$datavolpath/document_storage" "$datavolpath/db.sqlite3" \
+  "$datavolpath/settings"
